7e6ef4dca5961030d52a95e286919696c9dd1381
Mazharul Islam <abracadabraamm@gmail.com>
2025-03-08 01:40:30 +0600

    Thought-Process-Added

--- Arrays & Hashing/Two-Sum-Trouble.py
+++ Arrays & Hashing/Two-Sum-Trouble.py
@@ -9,11 +9,20 @@ PROBLEM DESCRIPTION:
 6. If no such pair exists, then PRINT -1.
 
 '''
+'''
 
+BRUTE FORCE ALGORITHM -> TC = O(n^2), SC = O(1)
 
+THOUGHT PROCESS:
+1. For each item in the list, find all its possible PAIRS
+2. ADD them up and see, if SUM == TARGETED-SUM
+3. If it is then print the DISTINCT INDICES and RETURN
+4. If not just continue with the LOOP
+5. Finally PRINT -1 when the loop ENDS
 
+'''
 
-# BRUTE FORCE ALGORITHM -> TC = O(n^2), SC = O(1)
+# BRUTE FORCE ALGORITHM
 
 def twoSum(arr, targetedSum):
     n = len(arr)
@@ -26,6 +35,20 @@ def twoSum(arr, targetedSum):
 
 
 
+'''
+
+THOUGHT PROCESS FOR BETTER SOLUTION:
+1. To achieve a better solution, we can think in terms of either TIME COMPLEXITY or SPACE COMPLEXITY
+2. Let's think in terms of TC. 
+3. Ok, what are the better TCs then O(n^2)?
+4. Can we think of an approach with O(n*logn) or O(n) or O(sqrt(n)) or maybe with O(1) time complexity?
+5. If we think a little bit, we will find out that there is an approach with O(n) TC if we use a HASHMAP (DICTIONARY in PYTHON)
+6. Initialize a HASHMAP
+7. For each element in the list find out the difference x between Targeted Sum & Current Element
+8. IF x EXISTS in the HASHMAP then we got what we're looking for (2 DISTINCT INDICES)
+9. ELSE we're going to put the (Current Element, Indices of Current Element) as (key, value) pair in the HASHMAP
+
+'''
 
 # A MORE BETTER APPROACH -> TC = O(n), SC = O(n)
 
