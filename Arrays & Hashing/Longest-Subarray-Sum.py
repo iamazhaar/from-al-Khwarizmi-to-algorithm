b617355af9d72362473631e55d4eabd9b50a91a2
Mazharul Islam <abracadabraamm@gmail.com>
2025-03-08 00:21:01 +0600

    Problem-Description-Added

---
+++ Arrays & Hashing/Longest-Subarray-Sum.py
@@ -0,0 +1,10 @@
+'''
+
+PROBLEM DESCRIPTION:
+
+1. You have an ARRAY OF INTEGERS
+2. You have an integer as SUBARRAY-SUM
+3. You have to find and print the LENGTH of the LONGEST CONTIGUOUS SUBARRAY whose SUM <= SUBARRAY-SUM
+4. If no subarray has SUM <= SUBARRAY-SUM then the answer is: 0
+
+'''
