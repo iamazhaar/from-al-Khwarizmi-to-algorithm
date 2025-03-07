be187f6ec3406e2413b1e50645b9581ba94e2423
Mazharul Islam <abracadabraamm@gmail.com>
2025-03-08 01:11:57 +0600

    Brute-Force-Algo-Written

--- Arrays & Hashing/Longest-Subarray-Sum.py
+++ Arrays & Hashing/Longest-Subarray-Sum.py
@@ -8,3 +8,41 @@ PROBLEM DESCRIPTION:
 4. If no subarray has SUM <= SUBARRAY-SUM then the answer is: 0
 
 '''
+'''
+
+BRUTE FORCE ALGORITHM -> TC = O(n^2), SC = O(1)
+
+THOUGHT PROCESS:
+1. Find the all possibile SUBARRAYS
+2. Calculate the SUM of all possible SUBARRAYS
+3. Initialize a MAX-LENGTH variable = 0
+4. For each SUBARRAY, If SUM <= SUBARRAY-SUM, update the MAX-LENGTH variable as MAX-LENGTH = max(MAX-LENGTH, CURRENT SUBARRAY LENGTH)
+5. Print the MAX-LENGTH variable
+
+'''
+
+# BRUTE FORCE ALGORITHM
+
+def longestSubarr(arr, targetedSum):
+    n = len(arr)
+    maxLen = 0
+    for i in range(n):
+        currentSubArrSum = 0
+        for j in range(i, n):
+            currentSubArrSum += arr[j]
+            if (currentSubArrSum <= targetedSum):
+                maxLen = max(maxLen, j-i+1)
+
+    print(f"The Longest Subarray of Length {maxLen} Can be Found With Sum Less Than or Equals to {targetedSum}")
+
+
+
+
+# For Testing Purpose
+'''
+
+A = [1, 1, 1, 1, 1]
+sum = 5
+longestSubarr(A, sum)
+
+'''
