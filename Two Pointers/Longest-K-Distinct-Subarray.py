fdf4bfd5af837fb8b3d7b9d271928befbed7879e
Mazharul Islam <abracadabraamm@gmail.com>
2025-11-12 00:30:27 +0600

    slidingWindow-description-edited

--- Two Pointers/Longest-K-Distinct-Subarray.py
+++ Two Pointers/Longest-K-Distinct-Subarray.py
@@ -1,21 +1,24 @@
 '''
 
 PROBLEM DESCRIPTION:
-1. You are given an ARRAY of integers of length N and an integer K
-2. Your task is to find the LENGTH of the longest contiguous subarray that contains at most K distinct elements.
+
+    1. You are given an ARRAY of integers of length N and an integer K
+    2. Find the LENGTH of the LONGEST CONTIGUOUS SUBARRAY that contains AT MOST K DISTINCT ELEMENTS
 
 
 THOUGHT PROCESS:
-1. This is basically a SLIDING WINDOW 2POINTER problem
-2. But I also need to use HASHMAP to keep track of the number of distinct elements I've got so far -> len(HASHMAP)
-3. Initialize two pointer both at index 0 and start traversing the ARRAY
-4. As long as distinctCount <= K -> update it's frequency in HASHMAP (expanding window)
-5. As distinctCount > K -> Keep shrinking the window untill distinctCount <= K
-6. Keep track of the MAX LENGHT at each iteration
-7. RETURN MAX LENGTH at the end of the loop
+
+    1. This is basically a TWO POINTER (SLIDING WINDOW) problem
+    2. But I also need to use HASHMAP to keep track of the number of distinct elements I've got so far -> len(HASHMAP)
+    3. Initialize two pointer both at index 0 and start traversing the ARRAY
+    4. As long as distinctCount <= K -> update it's frequency in HASHMAP (expanding window)
+    5. As distinctCount > K -> Keep shrinking the window untill distinctCount <= K
+    6. Keep track of the MAX LENGHT at each iteration
+    7. RETURN MAX LENGTH at the end of the loop
 
 
 TIME & SPACE COMPLEXITY:
+
     -> T.C. = O(n) 
     -> S.C. = O(k) [k=the highest number of distinct elements I can have]
 
--- Two Pointers/Longest-Subarray-Sum.py
+++ Two Pointers/Longest-Subarray-Sum.py
@@ -2,26 +2,31 @@
 
 PROBLEM DESCRIPTION:
 
-1. You have an ARRAY OF INTEGERS
-2. You have an integer as SUBARRAY-SUM
-3. You have to find and print the LENGTH of the LONGEST CONTIGUOUS SUBARRAY whose SUM <= SUBARRAY-SUM
-4. If no subarray has SUM <= SUBARRAY-SUM then the answer is: 0
+    1. You have an ARRAY OF INTEGERS
+    2. You have an integer as SUBARRAY-SUM
+    3. You have to find and print the LENGTH of the LONGEST CONTIGUOUS SUBARRAY whose SUM <= SUBARRAY-SUM
+    4. If no subarray has SUM <= SUBARRAY-SUM then the answer is: 0
+
+
+THOUGHT PROCESS OF BRUTE FORCE ALGORITHM:
+
+    1. For an array of N integers there're N*(N+1)/2 CONTIGUOUS SUBARRAY possible
+    2. Initialize MAX-LENGTH = 0
+    3. We'll find all possible CONTIGUOUS SUBARRAY and check if SUM <= TARGET_SUM
+    4. If so update MAX-LENGTH = max(MAX-LENGTH, CURRENT SUBARRAY LENGTH)
+    5. Print the MAX-LENGTH after checking all possible CONTIGUOUS SUBARRAY
+
+    
+ANALYSIS:
+
+    1. As I'm checking all the possible N*(N+1)/2 CONTIGUOUS SUBARRAY
+       Hence, Time Complexity: O(n^2)
+    2. As required auxiliary space doesn't increase with the size of the input
+       Hence, Space Complexity: O(1)
 
 '''
-'''
 
-BRUTE FORCE ALGORITHM -> TC = O(n^2), SC = O(1)
 
-THOUGHT PROCESS:
-1. Find the all possibile SUBARRAYS
-2. Calculate the SUM of all possible SUBARRAYS
-3. Initialize a MAX-LENGTH variable = 0
-4. For each SUBARRAY, If SUM <= SUBARRAY-SUM, update the MAX-LENGTH variable as MAX-LENGTH = max(MAX-LENGTH, CURRENT SUBARRAY LENGTH)
-5. Print the MAX-LENGTH variable
-
-'''
-
-# BRUTE FORCE ALGORITHM
 
 def longestSubarr(arr, targetedSum):
     n = len(arr)
@@ -39,20 +44,31 @@ def longestSubarr(arr, targetedSum):
 
 '''
 
-THOUGHT PROCESS FOR BETTER SOLUTION: 
-1. Let's try to get a better solution in terms of TC (You can think in terms of SC as well depending on your optimization critaria)
-2. Can we think of an solution with TCs as O(n*logn) or O(n) or O(sqrt(n)) or maybe O(1)?
-3. We can if we use the concepts of TWO-POINTERS. (NOTE: THIS IS A SLIDING WINDOW PROBLEM)
-4. First initialize two pointers -> LEFT & RIGHT
-5. Initialize MAX-LENGTH & CURRENT SUM to 0
-6. The idea is to maintain a valid subarray using two pointers: LEFT and RIGHT
-7. We keep expanding the RIGHT pointer to increase the MAX-LENGTH
-8. If CURRENT SUM exceeds SUBARRAY-SUM, we shrink from the LEFT to make it valid again
-9. Update MAX-LENGTH with the longest valid subarray found so far
+THOUGHT PROCESS OF OPTIMIZED SOLUTION: 
+
+    1. Let's try to get a better solution in terms of TC 
+       (You can think in terms of SC as well depending on your optimization critaria)
+    2. Can we think of an solution with TCs as O(n*logn) or O(n) or O(sqrt(n)) or maybe O(log n)?
+    3. We can if we use the concepts of TWO-POINTERS 
+       (This is a SLIDING WINDOW problem)
+    4. First initialize two pointers -> LEFT & RIGHT
+    5. Initialize MAX-LENGTH & CURRENT_SUM = 0
+    6. The idea is to maintain a valid subarray using two pointers: LEFT and RIGHT
+    7. We keep expanding the RIGHT pointer to increase the MAX-LENGTH
+    8. If CURRENT SUM exceeds SUBARRAY-SUM, we shrink from the LEFT to make it valid again
+    9. Update MAX-LENGTH with the longest valid subarray found so far
+
+
+ANALYSIS:
+
+    1. In the worst-case I'm basically traversing the array two times using those two pointers
+       Hence, Time Complexity: O(n)
+    2. As required auxiliary space doesn't increase with the size of the input
+       Hence, Space Complexity: O(1)
 
 '''
 
-# A MORE BETTER APPROACH -> TC = O(n), SC = O(1)
+
 
 def longestSubArr(arr, targetedSum):
     n = len(arr)
