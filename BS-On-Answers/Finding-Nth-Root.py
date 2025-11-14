abfc65c289c60eb456be308c1fa06b7266ff6481
Mazharul Islam <abracadabraamm@gmail.com>
2025-11-14 22:22:32 +0600

    problems-added

--- BS-On-Answers/Finding-Nth-Root.py
+++ BS-On-Answers/Finding-Nth-Root.py
@@ -2,20 +2,20 @@
 
 PROBLEM DESCRIPTION:
 
-1. You have two integer N, M 
-2. You have to find out the Nth root of M without the help of any built-in functions
-3. RETURN an INTEGER that is the Nth root of M
-4. If doesn't exist RETURN -1
+    1. You have two integer N, M 
+    2. You have to find out the Nth root of M without the help of any built-in functions
+    3. RETURN an INTEGER that is the Nth root of M
+    4. If doesn't exist RETURN -1
 
 
 THOUGHT PROCESS:
 
-1. This a BINARY SEARCH ON ANSWERS type problem
-2. I know the range of answers that is 1 to M
-3. Gonna apply BINARY SEARCH on the range(1, M) by assigning low = 1, high = M
-4. If (MID)^N == M -> RETURN MID
-5. If (MID)^N > M -> Eliminate the RIGHT HALF
-6. Otherwise eliminate the LEFT HALF
+    1. This a BINARY SEARCH ON ANSWERS type problem
+    2. I know the range of answers that is 1 to M
+    3. Gonna apply BINARY SEARCH on the range(1, M) by assigning low = 1, high = M
+    4. If (MID)^N == M -> RETURN MID
+    5. If (MID)^N > M -> Eliminate the RIGHT HALF
+    6. Otherwise eliminate the LEFT HALF
 
 '''
 
